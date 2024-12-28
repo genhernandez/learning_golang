cf3406e8520c0a3542160b73f80e32d6ae07492c
Genesis Hernandez <gendez2011@gmail.com>
2024-12-28 00:42:39 -0800

    Format currentTime to more readable format

--- hello_part2/hello.go
+++ hello_part2/hello.go
@@ -5,11 +5,13 @@ import (
 	"time"
 )
 
+const LayoutYYYYMMDDHHMMSS = "2006-01-02 15:04:05"
+
 func main() {
 	currentTime := time.Now()
 	helloString := "Hello, World!"
 	nameString := "My name is Genesis"
-	dateString := fmt.Sprintf("Current time:  %s", currentTime.String())
+	dateString := fmt.Sprintf("Current time:  %s", currentTime.Format(LayoutYYYYMMDDHHMMSS))
 	fmt.Println(helloString)
 	fmt.Println(nameString)
 	fmt.Println(dateString)
